331b5e57b5eb19b345326b75a8dc15e5754d9ac3
yaoyu bao <yaobao5354@gmail.com>
2019-08-20 22:10:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index{|name, index| puts "#{index+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|planet| #{
 end
 
 def long_planeteer_calls# code an argument here
