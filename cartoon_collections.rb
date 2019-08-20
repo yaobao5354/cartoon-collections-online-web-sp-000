1806f3413792fde25f72154908406c83306c37c8
yaoyu bao <yaobao5354@gmail.com>
2019-08-20 22:16:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  planet_array = array.collect{|planet| planet[0].upcase  
+  planet_array = array.collect{|planet| planet[0].upcase planet + "!"} 
 end
 
 def long_planeteer_calls# code an argument here
